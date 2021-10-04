0ea40f1105d11d1440efdebf699a6f491cdfd652
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-10-04 19:07:41 +0200

    Correctly calculate the fingerprint of the keys.

--- scripts/create_keys.sh
+++ scripts/create_keys.sh
@@ -2,6 +2,6 @@
 for I in $(seq 1 15); 
 do 
 	ssh-keygen -f keys/user${I}_id_rsa -b 4096 -N '' -C "Key for user $I"
-	ssh-keygen -l -E md5 -f keys/user1_id_rsa | grep -Eo "MD5:[0-9a-zA-Z:]+\b" | sed -e 's/MD5://' > keys/user${I}_id_rsa_fingerprint
+	ssh-keygen -l -E md5 -f keys/user${I}_id_rsa.pub | grep -Eo "MD5:[0-9a-zA-Z:]+\b" | sed -e 's/MD5://' > keys/user${I}_id_rsa_fingerprint
 	puttygen keys/user${I}_id_rsa -O private -o keys/user${I}_id_rsa.ppk
 done
